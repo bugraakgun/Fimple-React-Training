1285b5c6d2e46c49f3bdf1687952db122c06c49a
Buğra Akgün <bugraakgun8@outlook.com>
2024-01-02 03:28:05 +0300

    firebase added

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,7 +1,9 @@
-import React from 'react'
-
-export default function firebaseConfig() {
-  return (
-    <div>firebaseConfig</div>
-  )
-}
+export const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "patika-fimple-form-app.firebaseapp.com",
+    projectId: "patika-fimple-form-app",
+    storageBucket: "patika-fimple-form-app.appspot.com",
+    messagingSenderId: "650922857660",
+    appId: "1:650922857660:web:43af738859bd811bebacb9",
+    measurementId: "G-KWTRWDKEEB"
+};
