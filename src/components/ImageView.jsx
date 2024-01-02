81ed4b16e5a5d264cab6bdc399d423bc174e3f48
Buğra Akgün <bugraakgun8@outlook.com>
2024-01-02 03:39:02 +0300

    added ImageView component for form image

--- src/components/ImageView.jsx
+++ src/components/ImageView.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import noPhoto from "../assest/img/no-photo.jpg";
 
-export default function ImageView() {
-  return (
-    <div>ImageView</div>
-  )
+export default function ImageView({ imageUrl }) {
+  return imageUrl ? (
+    <Link to={imageUrl} target="_blank" className="relative inline-block group">
+      <img
+        src={imageUrl}
+        alt="img"
+        className="w-4/5 mx-auto my-4 transition-transform transform group-hover:scale-105"
+      />
+    </Link>
+  ) : (
+    <img
+      src={noPhoto}
+      alt="img"
+      className="w-4/5 mx-auto my-4 transition-transform transform hover:scale-105 cursor-pointer"
+    />
+  );
 }
