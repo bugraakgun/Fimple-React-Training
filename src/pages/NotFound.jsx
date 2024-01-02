ec270a6f47034413ed709ba77713213b99f25cee
Buğra Akgün <bugraakgun8@outlook.com>
2024-01-02 03:32:48 +0300

    404 Not Found Page added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Header from '../components/Header'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <>
+      <Header />
+      <div className="max-w-xl  mx-auto mt-8 p-6 bg-gray-100 rounded-md text-center break-words gap-4 flex flex-col">
+        <h2 className='text-red-600 font-bold text-xl'>404 - Aradığınız Sayfa Bulunamadı!</h2>
+        <span className='text-gray-700'>Adresi kontrol edip tekrar deneyiniz.</span>
+      </div>
+    </>
   )
 }
