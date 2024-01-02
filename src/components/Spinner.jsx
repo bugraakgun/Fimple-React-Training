c68047865a20a5624d7c660f5c5059e79ec80636
Buğra Akgün <bugraakgun8@outlook.com>
2024-01-02 03:38:18 +0300

    added Spinner component for loading

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { TbFidgetSpinner } from "react-icons/tb";
 
 export default function Spinner() {
   return (
-    <div>Spinner</div>
-  )
+    <>
+      <div className="absolute inset-0 bg-black opacity-20 z-10"></div>
+      <div className="absolute  -translate-x-1/2 -translate-y-1/2 top-2/4 left-1/2 z-20">
+        <TbFidgetSpinner className="animate-spin w-10 h-10" />
+      </div>
+    </>
+  );
 }
